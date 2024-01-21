3c5ae16ba44d4b85fb35b5f34078519fc0a406c1
Blake Hill <blake.h.hill09@gmail.com>
2024-01-20 20:15:20 -0700

    variables.js

--- sdlc.js
+++ sdlc.js
@@ -18,4 +18,16 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
+// Step 1
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+// step 2
+const Analysis = "These are the requirments set by the shareholders or stockholders for the benifit of the end-users, to difine the functinal and non-functional requirements."
+// step 3
+const Design = "The developmental phase is accomplished by detailing the system design, this will include the components and data flow points, if needed protorypes may be required to give a proof of concept."
+// step 4
+const Implementation = "Based on the first three steps, we will actually write and create the code following coding best practices."
+// step 5
+const Testing = "Run the code and system through a series of tests in order to fix issues that will occur, ensure that the software and code meets all requirements."
+// step 6
+const Maintain = "There will always be something that needs to be fixed or updated, due to this support and maintainence will be required"
+// I dont know if it matters to say it but i thought "const" would be best for this since the values wont be changing. 
